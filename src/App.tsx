2f8233bf597b5915520b6ea9a30cea7bd3f1ce14
EfrainRodriguez <efrainrg92@gmail.com>
2023-11-14 19:36:46 -0500

    adding absolute routes

--- src/App.tsx
+++ src/App.tsx
@@ -1,19 +1,7 @@
-import { Button, Avatar, Box, Typography } from '@mui/material';
+import ThemeConfig from '@/theme/ThemeConfig';
 
 const App = () => {
-  return (
-    <div>
-      <Button>Click me</Button>
-      <Button variant="outlined">Click me</Button>
-      <Button variant="contained">Click me</Button>
-      <Avatar alt="Remy Sharp" src="https://via.placeholder.com/150/92c952" />
-      <Typography variant="h1">Titutlo grande</Typography>
-      <Typography variant="h1" component="p">
-        Titutlo grande 2
-      </Typography>
-      <Box marginTop={3}>Este es un Box</Box>
-    </div>
-  );
+  return <ThemeConfig>App</ThemeConfig>;
 };
 
 export default App;
--- src/main.tsx
+++ src/main.tsx
@@ -1,13 +1,10 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
-import App from './App.tsx';
 
-import ThemeConfig from './theme/ThemeConfig.tsx';
+import App from './App.tsx';
 
 ReactDOM.createRoot(document.getElementById('root')!).render(
   <React.StrictMode>
-    <ThemeConfig>
-      <App />
-    </ThemeConfig>
+    <App />
   </React.StrictMode>
 );
